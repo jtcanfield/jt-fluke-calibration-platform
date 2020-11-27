e49103111a54650b1e4adefec251441d91ca0fcc
Jonathan Canfield <jonathantcanfield@gmail.com>
2020-11-27 16:57:47 -0500

    feat: add loading state and axios data fetching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('https://www.cubyt.io/data/categories');
+        setData(response.data);
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {loading && <h1>Loading...</h1>}
         <p>
           Edit
           {' '}
