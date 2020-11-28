6ae93ebaaa20709e19cb65632668126b2bb5c171
Jonathan Canfield <jonathantcanfield@gmail.com>
2020-11-27 21:01:06 -0500

    feat: add itemcard grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import {
+  AppBar, Container, Grid, Toolbar, Typography,
+} from '@material-ui/core';
 import axios from 'axios';
+import ItemCard from './components/ItemCard';
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,24 +27,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              Fluke
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <img src={logo} className="App-logo" alt="logo" />
-        {loading && <h1>Loading...</h1>}
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Container>
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          spacing={1}
+        >
+          {data && data.map((info) => (
+            <ItemCard
+              category_name={info.category_name}
+              display_name={info.display_name}
+              description={info.description}
+              image_uri={info.image_uri}
+              key={info.category_name}
+            />
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 }
